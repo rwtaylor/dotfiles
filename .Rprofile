d23732254f3a5062eff76869012f662258249a4c
Ryan W. Taylor <ryan@ryantaylor.net>
2015-06-30 15:50:17 -0700

    First commit

---
+++ .Rprofile
@@ -0,0 +1,10 @@
+
+if (interactive()) {
+  if (!suppressMessages(require(colorout)))
+    warning("colorout cannot be loaded...")
+}
+
+options(
+  width = 98,
+  repos=c("http://cran.cnr.Berkeley.edu","http://cran.stat.ucla.edu")
+)
---
+++ rpackages.R
@@ -0,0 +1,45 @@
+# Run this file to install all required packages (that aren't already installed.)
+
+# CRAN packages
+cran_pkgs <- c( "devtools", "MCMCglmm", "lme4", "foreach", "doMC", "ggplot2",
+  "ggthemes", "knitr", "pander", "grid", "effects", "arm", "dplyr", "tidyr",
+  "readr", "rmarkdown", "data.table")
+
+# Github packages (package name = github repository)
+github_pkgs <- list()
+
+# Packages on non-cran repositories
+non_cran_pkgs <- list(printr = c('http://yihui.name/xran', 'http://cran.rstudio.com'))
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Check for already installed packages
+installed_pkgs <- installed.packages()[ ,"Package"]
+
+# Install cran packages if not already installed
+new_cran_pkgs <- cran_pkgs[!cran_pkgs %in% installed_pkgs]
+
+if(length(new_cran_pkgs)){
+  install.packages(new_cran_pkgs)
+}
+
+
+# Install github packages if not already installed
+github_pkgs <- github_pkgs[!names(github_pkgs) %in% installed_pkgs]
+
+if(length(github_pkgs)){
+  for(i in 1:length(github_pkgs)){
+    devtools::install_github(github_pkgs[[i]])
+  }
+}
+
+
+# Install github packages if not already installed
+non_cran_pkgs <- non_cran_pkgs[!names(non_cran_pkgs) %in% installed_pkgs]
+
+if(length(non_cran_pkgs)){
+  for(i in 1:length(non_cran_pkgs)){
+    install.packages(names(non_cran_pkgs[i]), repos = non_cran_pkgs[[i]])
+   }
+}
