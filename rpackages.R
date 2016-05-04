40bb3287d43937a3d194aa501d77986763ba9199
Ryan W. Taylor <ryan@ryantaylor.net>
2016-05-04 13:01:45 -0700

    Add bioconductor

--- rpackages.R
+++ rpackages.R
@@ -1,13 +1,5 @@
 # Run this file to install all required packages (that aren't already installed.)
 
-# #Bioconductor
-# source("https://bioconductor.org/biocLite.R")
-#
-# library(biocLite)
-# biocLite("Biostrings")
-# biocLite("ShortRead")
-# biocLite("zlibbioc")
-
 # CRAN packages
 cran_pkgs <- c("devtools", "MCMCglmm", "lme4", "foreach", "doMC", "ggplot2",
   "ggvis", "ggthemes", "knitr", "pander", "grid", "effects", "arm", "dplyr",
@@ -20,6 +12,10 @@ github_pkgs <- list(colorout = "jalvesaq/colorout", ezsummary = "haozhu233/ezsum
 # Packages on non-cran repositories
 non_cran_pkgs <- list(printr = c('http://yihui.name/xran', 'http://cran.rstudio.com'))
 
+# Bioconductor
+source("https://bioconductor.org/biocLite.R")
+biocLite()
+bio_pkgs <- c("Biostrings", "ShortRead", "zlibbioc")
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -51,4 +47,11 @@ if(length(non_cran_pkgs)){
   for(i in 1:length(non_cran_pkgs)){
     install.packages(names(non_cran_pkgs[i]), repos = non_cran_pkgs[[i]])
    }
+}
+
+# Install cran packages if not already installed
+new_bio_pkgs <- bio_pkgs[!bio_pkgs %in% installed_pkgs]
+
+if(length(new_bio_pkgs)){
+  biocLite(new_bio_pkgs)
 }
